55ccafe59274618fe4f52ef819395efd56e894aa
Protap Barman <protapbarman02@gmail.com>
2024-05-14 21:58:17 +0530

    service module added

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'HomeController::index');
+
+$routes->get('/services', 'ServiceController::index');
+
+$routes->get('/services/create', 'ServiceController::create');
