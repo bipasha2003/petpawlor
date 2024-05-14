bc9a0b691a001788a2a31c3320d586fae01d3165
Protap Barman <protapbarman02@gmail.com>
2024-05-14 22:08:29 +0530

    prtoap

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/petpawlor/';
+    public string $baseURL = 'http://localhost/git_petpawlor/petpawlor/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
